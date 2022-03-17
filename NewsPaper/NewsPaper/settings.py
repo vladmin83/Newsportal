21eb5bbfa3cd18d62085dcc1bd6779bc39e51650
vladmin83 <vladikmin83@yandex.ru>
2022-03-18 00:24:34 +0300

    refact

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'NewsPaper.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'news/templates/news', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -21,7 +21,15 @@ class Author(models.Model):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=64, unique=True)
+    name = models.CharField(max_length=64, unique=True, verbose_name='категория')
+    discription = models.TextField(null=True, blank=True, verbose_name='описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
 
 class Post(models.Model):
@@ -34,10 +42,10 @@ class Post(models.Model):
         (ARTICLE, "Статья")
     )
     categoryType = models.CharField(max_length=2, choices=CATEGORY_CHOICES, default=ARTICLE)
-    dateCreation = models.DateTimeField(auto_now_add=True)
+    dateCreation = models.DateTimeField(auto_now_add=True, verbose_name='дата публикации')
     postCategory = models.ManyToManyField(Category, through="PostCategory")
-    title = models.CharField(max_length=128)
-    text = models.TextField()
+    title = models.CharField(max_length=128, verbose_name='заголовок')
+    text = models.TextField(null=True, blank=True, verbose_name='текст публикации')
     rating = models.SmallIntegerField(default=0)
 
     def like(self):
@@ -51,6 +59,13 @@ class Post(models.Model):
     def preview(self):
         return self.text[0:123] + '...'
 
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Публикация'
+        verbose_name_plural = 'Публикации'
+
 class PostCategory(models.Model):
     postThrough = models.ForeignKey(Post, on_delete=models.CASCADE)
     categoryThrough = models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -59,7 +74,7 @@ class PostCategory(models.Model):
 class Comment(models.Model):
     commentPost = models.ForeignKey(Post, on_delete=models.CASCADE)
     commentUser = models.ForeignKey(User, on_delete=models.CASCADE)
-    text = models.TextField()
+    text = models.TextField(null=True, blank=True)
     dateCreation = models.DateTimeField(auto_now_add=True)
     rating = models.SmallIntegerField(default=0)
 
